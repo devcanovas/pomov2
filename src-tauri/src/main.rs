9fd3bbfa39fd2bec4e20f54b4035473e8de43a09
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-22 17:55:23 -0300

    Implementing the initialization of app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,50 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{generate_context, Manager, SystemTray, SystemTrayEvent, Window};
+use tauri_plugin_positioner::{Position, WindowExt};
+
 fn main() {
     tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(|app| {
+            let splashscreen_window = app.get_window("splashscreen").unwrap();
+            let main_window = app.get_window("main").unwrap();
+            tauri::async_runtime::spawn(async move {
+                // configuration to when app starts, show the splashscreen and after 5 seconds,
+                // close and show the main window
+                splashscreen_window
+                    .move_window(Position::RightCenter)
+                    .unwrap();
+                std::thread::sleep(std::time::Duration::from_secs(5));
+                splashscreen_window.close().unwrap();
+
+                main_window.move_window(Position::RightCenter).unwrap();
+                main_window.show().unwrap();
+            });
+            Ok(())
+        })
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_id("Pomo"))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window: Window = app.get_window("main").unwrap();
+                window.move_window(Position::RightCenter).unwrap();
+                open_close_tray(window);
+            }
+            _ => {}
+        })
+        .run(generate_context!())
+        .expect("erro while running tauri application")
+}
+
+fn open_close_tray(window: Window) {
+    if window.is_visible().unwrap() {
+        window.hide().unwrap()
+    } else {
+        window.show().unwrap();
+        window.set_focus().unwrap();
+    }
 }
